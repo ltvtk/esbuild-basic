35a7e2583d5d9759f73aaaad1a0ffbc918db455f
ikeyboard <iosapps.vn@gmail.com>
2020-10-16 11:51:43 +0700

    firstcomit

---
+++ README.md
@@ -0,0 +1,6 @@
+## simple esbuild react
+-npm install
+-npm run build
+then open index.html in public folder
+## note
+use *.jsx instead of *.js
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <h1>Hello, world!</h1>
+    )
+}
+
+ReactDOM.render( <App/>,document.getElementById('root'));
